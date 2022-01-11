f47156ba423fc68637bc1ad03e3a7d4b6b7d4c2f
CatalinCurecheriu <88787486+CatalinCurecheriu@users.noreply.github.com>
2022-01-11 17:00:49 +0100

    Add files via upload

---
+++ Fattoriale/Fattoriale/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Fattoriale
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Inserisci un numero e avrai il suo fattoriale.");
+            int n = Convert.ToInt32(Console.ReadLine());
+            for (int i = n - 1; i > 0; i--)
+                n = n * i;
+            Console.WriteLine(n);
+
+        }
+    }
+}
